c075f8f15fe6cb13d163d95dec9e3e97c5873e2f
Jenifer Souza <jen.s_souza@yahoo.com>
2025-12-11 06:37:48 -0300

    feat: V0 of the website

--- src/pages/UsoECobertura/index.tsx
+++ src/pages/UsoECobertura/index.tsx
@@ -1,18 +1,24 @@
-import {useRef} from 'react';
+import {useCallback, useRef, useState} from 'react';
 import RasterLayer from '../../components/RasterLayer';
+import HoverValuePopup from '../../components/HoverValuePopUp';
 import mapBiomasColorFn from '../../utils/MapbiomasPallete';
 import GeoJsonLayer, {type GeoJsonLayerRef} from "../../components/ShapeFileLayer.tsx";
 import MapAnimation from "../../components/MapAnimation.tsx";
 
 export default function TempUseShow() {
     const jacareiLayerRef = useRef<GeoJsonLayerRef>(null);
+    const [lstGeoraster, setLstGeoraster] = useState<any>(null);
+
+    const handleLstReady = useCallback((gr: any) => {
+        setLstGeoraster(gr);
+    }, []);
 
     return (
         <>
             <GeoJsonLayer
                 ref={jacareiLayerRef}
                 layerId="jacarei"
-                dataUrl="src/data/limite_municipal_jacarei_2024.geojson"
+                dataUrl="limite_municipal_jacarei_2024.geojson"
                 style={{color: '#2A3E5B', weight: 2, fillOpacity: 0.1}}
             />
 
@@ -26,18 +32,22 @@ export default function TempUseShow() {
             />
 
             <RasterLayer
-                url="src/data/LULAC.tif"
+                url="LULAC.tif"
                 opacity={0.70}
                 colorFn={mapBiomasColorFn}
                 fitBounds={false}
             />
 
             <RasterLayer
-                url="src/data/LST.tif"
+                url="LST.tif"
                 opacity={0}
+                onReady={handleLstReady}
                 fitBounds={false}
             />
 
+            <HoverValuePopup
+                getGeoraster={() => lstGeoraster}
+            />
 
         </>
     );
